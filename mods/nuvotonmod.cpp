65d0037109af686a80b1567f5ec575b569861048
ploop <ploopgm@gmail.com>
2017-08-09 23:11:13 +0300

    ArcticFox connection support

--- mods/nuvotonmod.cpp
+++ mods/nuvotonmod.cpp
@@ -12,13 +12,15 @@ NuvotonMod::~NuvotonMod()
 
 bool NuvotonMod::devConnect()
 {
+
+  if (hid_init())
+    return false;
+
   hid_device_info *devs, *cur_dev;
-
-  d.portName.clear();
-
-  hid_init();
   devs = hid_enumerate(0x0, 0x0);
   cur_dev = devs;
+  d.portName.clear();
+
   while (cur_dev)
     {
       if (cur_dev->vendor_id == NUVOTON_VID &&
@@ -34,13 +36,107 @@ bool NuvotonMod::devConnect()
     }
   hid_free_enumeration(devs);
 
-  return d.portName == "HID";
+  if (d.portName != "HID")
+    return false;
 
+  handle = hid_open(NUVOTON_VID, NUVOTON_PID, NULL);
+  if (!handle)
+    {
+      qDebug() << "unable to open device";
+    return false;
+    }
+
+  hid_set_nonblocking(handle, 1);
+
+  //hid_exit();
+  return true;
 
 }
 
 deviceInfo NuvotonMod::getDeviceInfo()
 {
   return d;
+}
+
+MonitoringData NuvotonMod::getMonitorData()
+{
+  QByteArray command;
+  QDataStream commandStream(&command, QIODevice::WriteOnly);
+  commandStream.setByteOrder(QDataStream::LittleEndian);
+  commandStream
+         << quint8(0x66) // Команда мониторинга
+         << quint8(14)   // хз что
+         << quint32(0)   // arg1
+         << quint32(BLOCK_SIZE)  // arg2
+         << quint8('H')
+         << quint8('I')
+         << quint8('D')
+         << quint8('C');
+  // Контрольная сумма
+  quint32 cs = 0;
+  for (int i = 0; i < command.size(); i++)
+    {
+      cs += command.at(i);
+    }
+  // Записываем сумму, остальное нулями
+  commandStream << quint32(cs)
+         << quint64(0) << quint64(0) << quint64(0)
+         << quint64(0) << quint64(0) << quint32(0)
+         << quint8(0)  << quint8(0) ;
+
+  // Пишем в девайс
+  hid_write(handle, (quint8*)command.data(), command.size());
+
+  quint8 buf[BLOCK_SIZE];
+  memset(buf, 0, sizeof(buf));
+
+  // Читаем ответ
+  while (hid_read(handle, buf, BLOCK_SIZE) > 0);
+
+  QByteArray result;
+  QDataStream outStream(&result, QIODevice::ReadWrite);
+
+  for (int i = 0; i < BLOCK_SIZE; i++)
+    outStream << buf[i];
+
+  MonitoringData *d;
+  d = (MonitoringData *)&buf;
+
+  qDebug() << "Bat1_voltage" << d->bat1Voltage + 275;
+  qDebug() << "Bat2_voltage" << d->bat2Voltage + 275;
+  qDebug() << "Bat3_voltage" << d->bat3Voltage + 275;
+  qDebug() << "Bat4_voltage" << d->bat4Voltage + 275;
+
+  return *d;
+}
+
+double NuvotonMod::getBat1()
+{
+  MonitoringData d = getMonitorData();
+  return double((d.bat1Voltage + 275)) / double(100);
+}
+
+double NuvotonMod::getBat2()
+{
+  MonitoringData d = getMonitorData();
+  return double((d.bat2Voltage + 275)) / double(100);
+}
+
+double NuvotonMod::getBat3()
+{
+  MonitoringData d = getMonitorData();
+  return double((d.bat3Voltage + 275)) / double(100);
+}
+
+int NuvotonMod::batCount()
+{
+  // TODO Сделать
+  return 2;
+}
+
+bool NuvotonMod::devIsConnected()
+{
+  // TODO Сделать
+  return true;
 }
 
--- mods/nuvotonmod.h
+++ mods/nuvotonmod.h
@@ -7,6 +7,37 @@
 #define NUVOTON_VID  0x0416
 #define NUVOTON_PID  0x5020
 
+#define BLOCK_SIZE   64
+
+struct MonitoringData
+{
+  quint32 timeStamp;
+  bool isFiring;
+  bool isCharging;
+  bool isCelcius;
+
+  // Offsetted by 275, 420 - 275 = value
+  quint8 bat1Voltage;
+  quint8 bat2Voltage;
+  quint8 bat3Voltage;
+  quint8 bat4Voltage;
+
+  // X * 10
+  quint16 powerSet;
+  quint16 tempSet;
+  quint16 temp;
+
+  // X * 100
+  quint16 outVoltage;
+  quint16 outCurrent;
+
+  // X * 1000
+  quint16 resSet;
+  quint16 resREal;
+
+  quint8 boardTemp;
+} __attribute__ ((__packed__));
+
 class NuvotonMod : public CustomMod
 {
 public:
@@ -19,7 +50,22 @@ public:
   deviceInfo getDeviceInfo();
 
 private:
+
+  MonitoringData getMonitorData();
+
+
   deviceInfo d;
+  hid_device *handle;
+  MonitoringData data;
+
+  double getBat1();
+  double getBat2();
+  double getBat3();
+
+  int batCount();
+
+  bool devIsConnected();
+
 
 };
 
